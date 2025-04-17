09ed38c53949d13c0dbcf91156268d3132a83212
Pavithra-1803 <pavitrapavi1803@gmail.com>
2025-04-17 17:14:58 +0530

    Updated code

--- app/Home/Nav/MobileNav.tsx
+++ app/Home/Nav/MobileNav.tsx
@@ -1,30 +1,55 @@
+'use client';
+import React, { useEffect } from 'react';
 import Link from 'next/link';
-import React from 'react'
-import {  ImCross } from 'react-icons/im';
+import { ImCross } from 'react-icons/im';
 
-interface prop{
-  showNav:boolean
-  closeNav:()=>void
+interface Prop {
+  showNav: boolean;
+  closeNav: () => void;
 }
-const MobileNav = ({ closeNav, showNav }: prop) => {
+
+const MobileNav = ({ closeNav, showNav }: Prop) => {
+  // Prevent scrolling when menu is open
+  useEffect(() => {
+    document.body.style.overflow = showNav ? 'hidden' : 'auto';
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, [showNav]);
+
   if (!showNav) return null;
 
   return (
     <>
-      {/* overlay */}
-      <div className='fixed top-0 left-0 right-0 bottom-0 bg-black opacity-70 w-full h-[100vh] z-[10000]' />
+      {/* Overlay */}
+      <div className="fixed top-0 left-0 right-0 bottom-0 bg-black opacity-70 z-[10000]" />
 
-      {/* menu */}
-      <ul className='text-white justify-center flex fixed top-0 items-center flex-col h-[100vh] w-[60%] bg-blue-500 space-y-10 z-[10006]'>
-        <li className='navlink text-[25px]'><Link href="#">About</Link></li>
-        <li className='navlink text-[25px]'><Link href="#">Project</Link></li>
-        <li className='navlink text-[25px]'><Link href="#">Experience</Link></li>
-        <li className='navlink text-[25px]'><Link href="#">Education</Link></li>
-        <li className='navlink text-[25px]'><Link href="#">Contact</Link></li>
-        <ImCross onClick={closeNav} className="absolute top-[-1.4rem] right-[1.4rem] w-[1.4rem] h-[1.4rem] text-white cursor-pointer" />
+      {/* Mobile Menu */}
+      <ul className="fixed top-0 right-0 h-full w-[80%] max-w-xs bg-blue-500 z-[10006] text-white flex flex-col items-center justify-center space-y-10 px-4">
+        <li className="navlink text-2xl">
+          <Link href="#about" onClick={closeNav}>About</Link>
+        </li>
+        <li className="navlink text-2xl">
+          <Link href="#projects" onClick={closeNav}>Projects</Link>
+        </li>
+        <li className="navlink text-2xl">
+          <Link href="#experience" onClick={closeNav}>Experience</Link>
+        </li>
+        <li className="navlink text-2xl">
+          <Link href="#education" onClick={closeNav}>Education</Link>
+        </li>
+        <li className="navlink text-2xl">
+          <Link href="#contact" onClick={closeNav}>Contact</Link>
+        </li>
+
+        {/* Close Button */}
+        <ImCross
+          onClick={closeNav}
+          className="absolute top-5 right-5 w-6 h-6 text-white cursor-pointer"
+        />
       </ul>
     </>
   );
-}
+};
 
 export default MobileNav;
